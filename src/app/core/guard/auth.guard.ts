cfb81627a022ad5852d9da8c9a607d0a20f3b6ea
Rafael Rosa Moura <rafaelrosa01@hotmail.com>
2023-09-19 16:59:45 -0300

    Finalizado

---
+++ src/app/core/guard/auth.guard.ts
@@ -0,0 +1,12 @@
+import { CanActivateChildFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+
+export const AuthGuard: CanActivateChildFn = (childRoute, state) => {
+  const router = new Router;
+  if(!inject(AuthService).isAuthenticated()){
+    router.navigate(['']);
+    return false;
+  }
+  return true;
+};
